39c0499d3243a1928be35e01a92e59cfddf080b7
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-08-22 20:42:36 -0300

    add blocks in new-events

--- site/app/core/main/new-event/new-event.js
+++ site/app/core/main/new-event/new-event.js
@@ -1,9 +1,30 @@
 export default ngModule => {
   ngModule.controller('NewEventCtrl', function NewEventCtrl(firebaseAPIService, $state) {
+    const __ = require('underscore');
+    this.hours = [];
+    this.blocks = {};
+    this.currentBlock = 0;
+    this.startBlock = this.endBlock = '00:00';
+    this.generateHours = () => {
+      for ( let hour = 0; hour < 24; hour++) {
+        if (hour < 10) {
+          hour = `0${hour}`;
+        }
+        this.hours.push(`${hour}:00`);
+      }
+    };
+    this.deleteBlock = (obj) => {
+      const key = __.findKey(this.blocks, obj);
+      delete this.blocks[key];
+    };
+    this.addBlock = () => {
+      this.blocks[this.currentBlock] = {start: this.startBlock, end: this.endBlock};
+      this.currentBlock ++;
+    };
+    this.generateHours();
     firebaseAPIService.getDevices().then( (data) => {
       this.devices = data;
     });
-    this.hours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00'];
     this.submit = () => {
       firebaseAPIService.newEvent({title: this.form.title, archived: false, percent: this.form.percent}).then( (data) => {
         this.configDevices(data.key);
