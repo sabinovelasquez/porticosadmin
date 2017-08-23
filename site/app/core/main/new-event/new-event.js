2959af5e302af3e888e4748c3ece171904fe792d
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-08-23 00:47:54 -0300

    crear eventos: días en eventos, bloques por días, fecha para días, controladores y validador

--- site/app/core/main/new-event/new-event.js
+++ site/app/core/main/new-event/new-event.js
@@ -1,9 +1,14 @@
 export default ngModule => {
   ngModule.controller('NewEventCtrl', function NewEventCtrl(firebaseAPIService, $state) {
     const __ = require('underscore');
+    this.sending = false;
     this.hours = [];
+    this.days = {};
     this.blocks = {};
+    this.form = {};
+    this.form.percent = 80;
     this.currentBlock = 0;
+    this.enabled = false;
     this.startBlock = this.endBlock = '00:00';
     this.generateHours = () => {
       for ( let hour = 0; hour < 24; hour++) {
@@ -17,6 +22,19 @@ export default ngModule => {
       const key = __.findKey(this.blocks, obj);
       delete this.blocks[key];
     };
+    this.deleteDay = (obj) => {
+      const key = __.findKey(this.days, obj);
+      delete this.days[key];
+      if (this.getLength(this.days) === 0) {
+        this.enabled = false;
+      }
+    };
+    this.dateOptions = {
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1,
+    };
     this.addBlock = () => {
       this.blocks[this.currentBlock] = {start: this.startBlock, end: this.endBlock};
       this.currentBlock ++;
@@ -25,8 +43,26 @@ export default ngModule => {
     firebaseAPIService.getDevices().then( (data) => {
       this.devices = data;
     });
+    this.getLength = (obj) =>{
+      return Object.keys(obj).length;
+    };
+    this.addDay = () => {
+      const day = this.dayTitle.getDate();
+      const month = this.dayTitle.getMonth() + 1;
+      const year = this.dayTitle.getFullYear();
+      this.days[`${day}-${month}-${year}`] = {};
+      this.days[`${day}-${month}-${year}`].blocks = this.blocks;
+      this.days[`${day}-${month}-${year}`].date = this.dayTitle.toString();
+      this.blocks = {};
+      this.currentBlock = 0;
+      this.startBlock = this.endBlock = '00:00';
+      if (this.getLength(this.days) > 0) {
+        this.enabled = true;
+      }
+    };
     this.submit = () => {
-      firebaseAPIService.newEvent({title: this.form.title, archived: false, percent: this.form.percent}).then( (data) => {
+      this.sending = true;
+      firebaseAPIService.newEvent({title: this.form.title, archived: false, percent: this.form.percent, days: this.days}).then( (data) => {
         this.configDevices(data.key);
       });
     };
