2dd6c2d69bd2759161338f0eeb0c08b93e9a6468
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-08-24 23:17:34 -0300

    view event in progress

--- site/app/core/main/view-event/view-event.js
+++ site/app/core/main/view-event/view-event.js
@@ -1,11 +1,13 @@
 export default ngModule => {
   ngModule.controller('ViewEventCtrl', function ViewEventCtrl(firebaseAPIService, $stateParams) {
     this.eventKey = $stateParams.key;
+    this.loading = true;
     this.devices = {};
     firebaseAPIService.getEvent(this.eventKey).then( (data) => {
       this.event = data;
     });
     firebaseAPIService.getDevices().then( (data) => {
+      this.loading = false;
       angular.forEach(data, (device, key) => {
         if ( device.settings.eventKey === this.eventKey) {
           this.devices[key] = device;
