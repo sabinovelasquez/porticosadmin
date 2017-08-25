d0d1f9d564300f019fc5b5c95b777b934fb9f6c8
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-08-25 02:46:52 -0300

    download excel (WIP)

--- site/app/core/main/view-event/view-event.js
+++ site/app/core/main/view-event/view-event.js
@@ -1,10 +1,16 @@
 export default ngModule => {
   ngModule.controller('ViewEventCtrl', function ViewEventCtrl(firebaseAPIService, $stateParams) {
     this.eventKey = $stateParams.key;
+    const __ = require('underscore');
     this.loading = true;
+    this.excel = [];
     this.devices = {};
+    this.headers = ['Código', 'Fecha', 'Bloque', 'Hora', 'Sala'];
     firebaseAPIService.getEvent(this.eventKey).then( (data) => {
       this.event = data;
+      __.each(data.registers, (user) => {
+        this.excel.push(user);
+      });
     });
     firebaseAPIService.getDevices().then( (data) => {
       this.loading = false;
