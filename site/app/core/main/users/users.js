0e4706b5ae3bbb63aa510910ff1b6efc1b5d5669
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-08-29 08:16:37 -0300

    update user table

--- site/app/core/main/users/users.js
+++ site/app/core/main/users/users.js
@@ -1,14 +1,17 @@
 export default ngModule => {
-  ngModule.controller('UsersCtrl', function UsersCtrl($state, Auth) {
+  ngModule.controller('UsersCtrl', function UsersCtrl($state, Auth, firebaseAPIService) {
     this.user = {};
     this.loading = false;
-
+    firebaseAPIService.getUsers().then( (data) => {
+      this.users = data;
+    });
     this.createUser = () => {
       this.loading = true;
       this.error = '';
       Auth.$createUserWithEmailAndPassword(this.user.email, this.user.password).then( (firebaseUser) => {
         this.userCreated = firebaseUser;
         this.loading = false;
+        firebaseAPIService.storeUser(firebaseUser.uid, {email: firebaseUser.email});
       }).catch( (error) => {
         this.error = error;
         this.loading = false;
